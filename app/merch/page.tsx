b25670ad9f94da0aba950fa62322baf6a461a76a
K-sama <drumer19861018@gmail.com>
2024-05-21 17:32:06 +1000

    revalidate added in Merch SSG

--- app/merch/page.tsx
+++ app/merch/page.tsx
@@ -12,7 +12,10 @@ type Product = {
 };
 
 const fetchProducts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{
+    next: { revalidate: 60 }
+  } );
+  
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
